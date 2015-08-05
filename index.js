ca69c6f010892e29c527ea812d39f53314bc85b0
Prashanth Chandra <coolshanth94@gmail.com>
2015-08-05 15:52:00 +0800

    Add getPosts

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var subreddit = process.argv[2] || 'aww';
+var category = process.argv[3] || 'hot';
+var num = process.argv[4] || '25';
+var url = 'http://www.reddit.com/r/' 
+  + subreddit + '/' 
+  + category + '.json'
+  + '?limit=' + num;
+
+function getPosts(cb) {
+  http.get(url, res => {
+    var str = '';
+    res.on('data', d => str += d);
+    res.on('end', () => {
+      var parsed = JSON.parse(str);
+      parsed.data.children.map(cb);
+    });
+  }).on('error', err => console.error(err));
+}
+
+getPosts(console.log);
