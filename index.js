18aafd699aaa204ce8b3a605af24f69bb089b0b2
Prashanth Chandra <coolshanth94@gmail.com>
2015-10-22 11:01:57 +0800

    Use async.parallel
    Download images from Imgur gallery urls. Figure out file extension when it is missing from url.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
+
 const fs = require('fs');
+const mime = require('mime');
 const path = require('path');
-const http = require('http');
+const request = require('request');
 const async = require('async');
-const Stream = require('stream').Transform;
 const chalk = require('chalk');
 
 /* 
- * This is the main file, but CLI argument handling 
- * and creating an executable are delegated to cli.js
+ * Exports module to be called by cli.js, which is our executable
  */
 module.exports = (sub, cat, num) => {
   const url = 'http://www.reddit.com/r/' + 
@@ -23,31 +23,33 @@ module.exports = (sub, cat, num) => {
  * GETs JSON data, then calls getImage on each post
  * Use try/catch since Reddit sends an HTML error page instead of JSON if server is under load
  */
-function getPosts(url, cb) {
-  http.get(url, res => {
-    let body = '';
-    res.on('data', d => body += d);
-    res.on('end', () => {
-      try {
-        const parsed = JSON.parse(body);
-        async.each(parsed.data.children, cb);
-      } catch (e) {
-        console.log(e.message);
-        return;
-      }
-    });
-  }).on('error', e => console.log(e.message));
+function getPosts(url) {
+  request(url, (err, res, body) => {
+    try {
+      const parsed = JSON.parse(body);
+      async.parallel(parsed.data.children.map((link) => {
+        return function(callback) {
+          getImage(link, callback);
+        }
+      }),
+      function(err, results) {
+        console.log('All downloads complete');
+        console.log(`Downloaded ${results.filter((res) => res).length} out of ${results.length} links`);
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
 }
 
 /*
  * Download image if post url is a recognized image url, or do nothing
  * Rigorous tests are needed since an invalid url may slow down or crash the program
- * If url isn't an Imgur image, and doesn't end with .jpg/.png/.gif, or is 'https',
- * then it does nothing
+ * If url isn't an Imgur image, and doesn't end with .jpg/.png/.gif, then it does nothing
  */
-function getImage(post) {
+function getImage(post, callback) {
   const url = post.data.url;
-  const imgurImageRegex = /^https?\:\/\/(i\.)?imgur\.com\/([A-Z0-9]{5,8})((?:\.jpg)|(?:\.gifv)|(?:\.png)|(?:\.gif))?(?:\?1)?$/i;
+  const imgurImageRegex = /^https?\:\/\/(i\.)?imgur\.com\/(?:gallery\/)?([A-Z0-9]{5,8})((?:\.jpg)|(?:\.gifv)|(?:\.png)|(?:\.gif))?(?:\?1)?$/i;
   const imgurAlbumRegex = /^http\:\/\/imgur\.com\/a\/[a-zA-Z0-9]{5}/;
   const genericJpgOrPngOrGifRegex = /http:\/\/.*\/([^\/]+\.(jpg|png|gif))$/;
   let imgurImageMatch;
@@ -58,43 +60,42 @@ function getImage(post) {
   let ext;
  
   if (imgurImageMatch = url.match(imgurImageRegex)) {
-    // Imgur has a special download page, which is quite useful since their image urls
-    // vary heavily, e.g. url doesn't end with .jpg or has ?1 at the end
+    // Imgur has a special download page, since Imgur urls vary a lot
     downloadUrl = 'http://imgur.com/download/' + imgurImageMatch[2];
-    ext = imgurImageMatch[3] || '.jpg';
-    filename = imgurImageMatch[2] + ext;
+    filename = imgurImageMatch[2];
+    ext = imgurImageMatch[3];
+    // Extra logic to determine file extension when it isn't available in the url
+    if (!ext) {
+      request({url: downloadUrl, method: 'HEAD'}, (err, res, body) => {
+        ext = '.' + mime.extension(res.headers['content-type']);
+        downloadImage(downloadUrl, filename + ext);
+      });
+    }
+    else {
+      downloadImage(downloadUrl, filename + ext, callback);
+    }
+    return;
   }
   else if (genericJpgOrPngOrGifMatch = url.match(genericJpgOrPngOrGifRegex)) {
     downloadUrl = url;
     filename = genericJpgOrPngOrGifMatch[1];
+    downloadImage(downloadUrl, filename, callback);
+    return;
   }
   else if (imgurAlbumMatch = url.match(imgurAlbumRegex)) {
     console.log(url, chalk.cyan(' Imgur albums'), chalk.red(' not supported'));
-    return;
-  }
-  else if (~url.indexOf('imgur.com/gallery')) {
-    console.log(url, chalk.cyan(' Imgur galleries'), chalk.red(' not supported'));
-    return;
-  }
-  else if (~url.indexOf('https')) {
-    console.log(url, chalk.cyan(' https'), chalk.red(' not supported'));
-    return;
   }
   else {
     console.log(url, chalk.red(' Unrecognized image url'));
-    return;
   }
+  callback(null, null);
+}
 
-  const req = http.request(downloadUrl, res => {
-    const data = new Stream();
-    res.on('data', chunk => data.push(chunk));
-    res.on('end',
-      () => fs.writeFile(filename, 
-        data.read(), 
-        () => console.log(url, chalk.green(' downloaded successfully'))
-      )
-    );
-  });
-  req.on('error', e => console.log(e.message))
-  req.end();
+function downloadImage(url, filename, callback) {
+  request(url)
+      .pipe(fs.createWriteStream(filename))
+      .on('close', () => {
+        console.log(url, chalk.green(' downloaded successfully'));
+        callback(null, filename);
+      });
 }
